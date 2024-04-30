84f033a6192eac2ddcd7607ea84d3a339189e55c
N-Botirovna <nargizatosheva20@gmail.com>
2024-04-30 21:23:27 +0500

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from "react";
+import React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0);
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
+import './App.css';
 
-  return (
-    <>
-      <h1 className="text-red-600">Hello</h1>
-    </>
-  );
-}
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+);
 
 export default App;
